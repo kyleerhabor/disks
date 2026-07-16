e27468a0218f7d77bdc6761cd39b5b80c7e24c0c
Kyle Erhabor <kyleerhabor@gmail.com>
2026-07-16 00:47:44 -0400

    Initial Commit

---
+++ Disks/DisksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisksApp.swift
+//  Disks
+//
+//  Created by Kyle Erhabor on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct DisksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisksTests/DisksTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisksTests.swift
+//  DisksTests
+//
+//  Created by Kyle Erhabor on 7/16/26.
+//
+
+import Testing
+@testable import Disks
+
+struct DisksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
